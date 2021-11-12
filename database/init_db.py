70035e58fbdce29dec28afdcbf600c566c9d6d69
Francesco Conforti <82725386+checcoconf@users.noreply.github.com>
2021-11-12 22:14:25 +0100

    Delete init_db.py

--- database/init_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-connessione = sqlite3.connect("database.db")
-with open('crea_posts.sql') as f:
-    connessione.executescript(f.read())
-connessione.commit()
-connessione.close()    
